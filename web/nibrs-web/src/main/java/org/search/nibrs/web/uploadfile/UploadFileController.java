d113876a629921da5632ecb5277b2e77abb86ef3
Haiqi Wei <haiqi@search.org>
2020-02-13 13:22:12 -0600

    Make the PCT tool's welcome header message configurable.

--- web/nibrs-web/src/main/java/org/search/nibrs/web/uploadfile/UploadFileController.java
+++ web/nibrs-web/src/main/java/org/search/nibrs/web/uploadfile/UploadFileController.java
@@ -49,7 +49,6 @@ import org.springframework.security.web.authentication.logout.SecurityContextLog
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -72,11 +71,6 @@ public class UploadFileController {
 	
 	final List<String> acceptedFileTypes = 
 			Arrays.asList("application/zip", "text/plain", "application/octet-stream", "text/xml", "application/xml");
-	
-	@ModelAttribute("showUserInfoDropdown")
-	public Boolean getShowUserInfoDropDown(){
-		return appProperties.getShowUserInfoDropdown(); 
-	}
 	
 	@GetMapping("/")
 	public String getFileUploadForm(Model model) throws IOException {
